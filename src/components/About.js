2f884bc6951f9fcac0ce132ea37993a83fa7ef25
Cole Hartkopp <chsoccer4@outlook.com>
2023-06-12 19:14:36 -0500

    more accurate profile picture and email added to about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,4 +1,4 @@
-import logo from "../img/coleDress.png"
+import logo from "../img/Profile1mg.jpg"
 
 function About() {
   return (
@@ -6,7 +6,13 @@ function About() {
       <aside>
         <img src={logo} alt="Cole Hartkopp"/>
       </aside>
-      <p className="about-p">Cole Hartkopp is an enthusiastic and detail-oriented recent bootcamp gradutate with a strong desire to learn and grow in the technology industry. Possessing excellent communication, problem-solving, and time management skills, as well as proficiency in multiple full-stack languages. Cole has a desire to work as a full stack developer, but has very strong back end development skills with SQL, NoSQL, and Cloud databases. He is a dedicated team player who is adaptable, dependable, and committed to delivering high-quality work. Please navigate to the projects to view recent works, download my resume and contact me if you have any questions.</p>
+      <p className="about-p">Cole Hartkopp is an enthusiastic and detail-oriented recent bootcamp gradutate with a strong desire to learn and grow in the technology industry. Possessing excellent communication, problem-solving, and time management skills, as well as proficiency in multiple full-stack languages. Cole has a desire to work as a full stack developer, but has very strong back end development skills with SQL, NoSQL, and Cloud databases. He is a dedicated team player who is adaptable, dependable, and committed to delivering high-quality work. Please navigate to the projects to view recent works, download my resume and email me if you have any questions.</p>
+      <div className="about-c">
+        <h4>Contact Me</h4>
+        <ul>
+          <li>chartkopp@outlook.com</li>
+        </ul>
+      </div>
     </section>
   );
 }
